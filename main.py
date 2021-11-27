ce5f9e3a6b17d73ca6dd01cd36dad4e7a9c09bc2
ZenithClown <debmalyapramanik.005@gmail.com>
2021-11-27 15:33:21 +0530

    fix indentation

--- main.py
+++ main.py
@@ -3,5 +3,5 @@
 from random import randint
 
 if __name__ == "__main__":
-  li = [randint(0, 10) for _ in range(5)]
-  print(f"Random Generated List: {li}")
+    li = [randint(0, 10) for _ in range(5)]
+    print(f"Random Generated List: {li}")
